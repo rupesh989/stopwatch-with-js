3229cc70b9cf5c7ba9420a09cc89266ff680c1de
rupesh989 <rupeshbavaskar24>
2024-01-16 21:57:22 +0530

    updated

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let stopBtn = document.getElementById('stopBtn');
 let startBtn = document.getElementById('startBtn');
 let resetBtn = document.getElementById('resetBtn');
 
-let msec = 0o0; //use 0 or 0o0 
-let secs = 0o0;
-let mins = 0o0;
+let msec = 00;
+let secs = 00;
+let mins = 00;
 
 let timerId = null;
 
@@ -21,5 +21,27 @@ stopBtn.addEventListener('click', function(){
 });
 
 resetBtn.addEventListener('click', function(){
-
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 00;
 });
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+
+}
