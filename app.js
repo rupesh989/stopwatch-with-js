c8f9fef1986ff7f4a0bb324b96b448a17b1de9a1
rupesh989 <rupeshbavaskar24>
2024-01-16 21:41:56 +0530

    updated style and js file

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("working !!")
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 0o0;
+let secs = 0o0;
+let mins = 0o0;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00 : 00 : 00`;
+    msec = secs = mins = 0o0;
+});
